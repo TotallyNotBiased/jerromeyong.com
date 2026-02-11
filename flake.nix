d5dc9d5dd4fba0383f67684477e90d40094167d3
TotallyNotBiased <highfunctioningpsychopath@gmail.com>
2026-02-11 15:58:00 +1100

    initial commit with nix env for zola

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description ="nix flake for my personal site, jerromeyong.com";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zola
+          ];
+
+          shellHook = ''
+            echo "--------------------------------------------------"
+            echo "loaded zola env"
+            echo "'zola serve --interface 0.0.0.0' to preview on wsl"
+            echo "--------------------------------------------------"
+          '';
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "my-zola-site";
+          src = ./.;
+
+          buildInputs = [ pkgs.zola ];
+
+          buildPhase = ''
+            zola build
+          '';
+
+          # nix store output
+          installPhase = ''
+            mkdir -p $out
+            cp -r public/* $out/
+          '';
+        };
+      }
+    );
+}
